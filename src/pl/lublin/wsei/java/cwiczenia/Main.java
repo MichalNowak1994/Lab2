bb1687cdd146337d2a18f3c7c0a6587e196482a6
MichalNowak1994 <7xaatsfu>
2021-04-23 19:19:54 +0200

    Próba lab2

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,11 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+public class Main
+{
+    Account acc = new Account();
+    acc.setName = ("piotr Gołąbek");
+    System.out.println(acc.getName());
+}
